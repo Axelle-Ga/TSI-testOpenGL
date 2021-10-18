81dbd68c332e13b27d0ce5cc2d5adf3d0cdc2824
Axelle-Ga <axelle.gaige@gmail.com>
2021-10-18 16:24:22 +0200

    Episode 11 Uniform

--- main.cpp
+++ main.cpp
@@ -135,7 +135,8 @@ int main(void)
 
 
     /* Make the window's context current */
-    GlCall(glfwMakeContextCurrent(window));
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     /* Init Glew */
     if(glewInit()!= GLEW_OK){
@@ -179,15 +180,32 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GlCall(glUseProgram(shader));
 
+    GlCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GlCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         GlCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        //Change color
+        GlCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         //Draw the triangle
         GlCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr ));
 
+        if (r>1.0f)
+        {
+            increment = -0.05f;
+        }
+        else if (r< 0.0f)
+        {
+            increment = 0.05f;
+        }
+        r+= increment;
 
         /* Swap front and back buffers */
         GlCall(glfwSwapBuffers(window));
